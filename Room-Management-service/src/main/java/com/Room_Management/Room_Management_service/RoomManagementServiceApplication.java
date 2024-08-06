0d2093becca0407f02b980c0e172b8165ac481f2
Ravihara De Silva <117923473+Ravihara2000@users.noreply.github.com>
2024-08-06 09:31:03 +0530

    booking service initial commit

---
+++ Room-Management-service/src/main/java/com/Room_Management/Room_Management_service/RoomManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.Room_Management.Room_Management_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomManagementServiceApplication.class, args);
+	}
+
+}
---
+++ Room-Management-service/src/test/java/com/Room_Management/Room_Management_service/RoomManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Room_Management.Room_Management_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
